5c1be847c917daa7723619afa551f786dfb95c35
Nguyen, Victor <vdn140030@gmail.com>
2020-12-07 21:48:30 -0600

    feat(Hero.js, index.js): add Hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import React from "react"
+import styled from "styled-components"
+
+const Hero = () => {
+  return (
+  <HeroContainer>
+    <HeroBg>
+      <VideoBg src="" type="video/mp4" />
+    </HeroBg>
+  </HeroContainer>
+  )
+}
+
+export default Hero;
+
+const HeroContainer = styled.div``
+
+const HeroBg = styled.div``
+
+const VideoBg = styled.video``
