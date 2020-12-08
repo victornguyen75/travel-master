e82421c1a69c2da289ba881c6f8ab70e29c0906e
Nguyen, Victor <vdn140030@gmail.com>
2020-12-07 22:26:24 -0600

    feat(Header.js, Hero.js): update Hero and Header sections

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
 export default Header
 
 const Nav = styled.nav`
-  background: purple;
+  background: transparent;
   height: 80px;
   display: flex;
   justify-content: space-between;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import { Button } from "./Button"
 
 const Hero = () => {
   return (
@@ -7,14 +8,39 @@ const Hero = () => {
     <HeroBg>
       <VideoBg src="" type="video/mp4" />
     </HeroBg>
+    <HeroContent>
+      <HeroItems>
+        <HeroH1>Unforgettable Experiences</HeroH1>
+        <HeroP>Sights to be Seen</HeroP>
+        <Button>Travel Now!</Button>
+      </HeroItems>
+    </HeroContent>
   </HeroContainer>
   )
 }
 
 export default Hero;
 
-const HeroContainer = styled.div``
+const HeroContainer = styled.div`
+  color: #fff;
+  background: #0c0c0c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 0 1rem;
+  position: relative;
+  margin-top: -80px;
+`
 
 const HeroBg = styled.div``
 
 const VideoBg = styled.video``
+
+const HeroContent = styled.div``
+
+const HeroItems = styled.div``
+
+const HeroH1 = styled.h1``
+
+const HeroP = styled.p``
