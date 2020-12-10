acef01ad51a3f8220a6dce51404e946eabe90b7b
Nguyen, Victor <vdn140030@gmail.com>
2020-12-10 11:12:31 -0600

    feat(beach.mp4, Hero.js): use a new video

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "styled-components"
 import { Button } from "./Button"
-import Video from "../assets/videos/city.mp4"
+import Video from "../assets/videos/beach.mp4"
 
 const Hero = () => {
   return (
