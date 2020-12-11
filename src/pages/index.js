26cc4b82f09fac5f07ebb376862084fa049b623c
Nguyen, Victor <vdn140030@gmail.com>
2020-12-10 22:19:52 -0600

    feat(Email.js, index.js): add Email section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Hero from "../components/Hero"
 import Trips from "../components/Trips"
 import Testimonials from "../components/Testimonials";
 import Stats from "../components/Stats";
+import Email from "../components/Email"
 
 const IndexPage = () => (
   <Layout>
@@ -13,6 +14,7 @@ const IndexPage = () => (
     <Trips heading="Our Favorite Destinations"/>
     <Testimonials />
     <Stats />
+    <Email />
   </Layout>
 )
 
